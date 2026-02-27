b4aa93754490631ceedbe707110f208cc70869dc
psamson0v <131488830+psamson0v@users.noreply.github.com>
2026-02-27 13:44:13 -0500

    only create a new branch if the PR was successful

--- check_git.sh
+++ check_git.sh
@@ -28,26 +28,29 @@ fi
 if [ "$1" = "push" ]; then
 
     branch=$(git rev-parse --abbrev-ref HEAD)
-    case "$branch" in
     # If the branch is followed by "stack-abcd1234-X" then it's already a part of a stack
     # increment the number X and create a new branch
+    case "$branch" in
         *stack-????????-[0-9]*)
-            gh pr create --fill -B $branch
-            # TODO: put the stack number in the body of the PR somewhere
-            num=$(printf '%s' "$branch" | sed 's/.*-stack-.\{8\}-//')
-            prefix=$(printf '%s' "$branch" | sed 's/-[0-9]*$//')
-            new_branch="${prefix}-$((num + 1))"
-            git checkout -b "$new_branch"
+            if gh pr create --fill -B $branch; then
+                # TODO: put the stack number in the body of the PR somewhere
+                num=$(printf '%s' "$branch" | sed 's/.*-stack-.\{8\}-//')
+                prefix=$(printf '%s' "$branch" | sed 's/-[0-9]*$//')
+                new_branch="${prefix}-$((num + 1))"
+                git checkout -b "$new_branch"
+            fi
             ;;
     # If the branch is not followed by "stack-abcd1234-X" then it's not a part of a stack yet. 
     # the new branch should have a stack identifier
         *)
-            gh pr create --fill
-            # TODO: put the stack number in the body of the PR somewhere    
-            rand=$(LC_ALL=C tr -dc 'a-z0-9' < /dev/urandom | head -c 8)
-            new_branch="${branch}-stack-${rand}-1"
-            git checkout -b "$new_branch"
+            if gh pr create --fill; then
+                # TODO: put the stack number in the body of the PR somewhere    
+                rand=$(LC_ALL=C tr -dc 'a-z0-9' < /dev/urandom | head -c 8)
+                new_branch="${branch}-stack-${rand}-1"
+                git checkout -b "$new_branch"
+            fi
             ;;
     esac
 fi
 
+#this is a comment at the base of the stack

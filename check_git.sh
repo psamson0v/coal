220795c2dcdf39802e864ddd538994a548b01095
psamson0v <131488830+psamson0v@users.noreply.github.com>
2026-02-27 13:15:02 -0500

    Adding comments

--- check_git.sh
+++ check_git.sh
@@ -24,17 +24,23 @@ if ! gh auth status > /dev/null 2>&1; then
     gh auth login
 fi
 
+# Push your current changes to a new or existing stack
 if [ "$1" = "push" ]; then
     gh pr create --fill
+    # TODO: put the stack number in the body of the PR somewhere
 
     branch=$(git rev-parse --abbrev-ref HEAD)
     case "$branch" in
+    # If the branch is followed by "stack-abcd1234-X" then it's already a part of a stack
+    # increment the number X and create a new branch
         *stack-????????-[0-9]*)
             num=$(printf '%s' "$branch" | sed 's/.*-stack-.\{8\}-//')
             prefix=$(printf '%s' "$branch" | sed 's/-[0-9]*$//')
             new_branch="${prefix}-$((num + 1))"
             git checkout -b "$new_branch"
             ;;
+    # If the branch is not followed by "stack-abcd1234-X" then it's not a part of a stack yet. 
+    # the new branch should have a stack identifier
         *)
             rand=$(LC_ALL=C tr -dc 'a-z0-9' < /dev/urandom | head -c 8)
             new_branch="${branch}-stack-${rand}-1"
